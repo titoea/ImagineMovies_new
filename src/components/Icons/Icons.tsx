58bf4ea4bace6f75a74e5b4c536754d598836d39
titoeffiongakpan <tito@flutterwavego.com>
2024-11-04 08:13:10 +0100

    feat(icons): add nav drawer icons

--- src/components/Icons/Icons.tsx
+++ src/components/Icons/Icons.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import HamburgerSVG from '../../assets/icons/Hamburger.svg';
+import HomeSVG from '../../assets/icons/home.svg';
+import ReservationsSVG from '../../assets/icons/reservations.svg';
+import SettingsSVG from '../../assets/icons/settings.svg';
+import HelpSVG from '../../assets/icons/help.svg';
 import {IconProps} from './interfaces';
 
 export const HamburgerIcon: React.FC<IconProps> = function HamburgerIcon({
@@ -10,10 +14,56 @@ export const HamburgerIcon: React.FC<IconProps> = function HamburgerIcon({
   // const {themeColors} = useTheme();
   return (
     <HamburgerSVG
-      fill={color}
+      fill={color || 'blue'}
       width={size || 16}
       height={size || 16}
       opacity={opacity || 1}
+    />
+  );
+};
+
+export const HomeIcon: React.FC<IconProps> = function HomeIcon({
+  color, size, opacity,
+}) {
+  return(
+    <HomeSVG
+    fill={color || 'blue'}
+    width={size || 16}
+    height={size || 16}
+    opacity={opacity || 1}
+    />
+  );
+};
+
+export const ReservationsIcon: React.FC<IconProps> = function ReservationsIcon({ color, size, opacity}){
+  return (
+    <ReservationsSVG
+    fill={color || 'blue'}
+    width={size || 16}
+    height={size || 16}
+    opacity={opacity || 1}
+    />
+  );
+};
+
+export const SettingsIcon: React.FC<IconProps> = function SettingsIcon({ color, size, opacity}){
+  return (
+    <SettingsSVG
+    fill={color || 'blue'}
+    width={size || 16}
+    height={size || 16}
+    opacity={opacity || 1}
+    />
+  );
+};
+
+export const HelpIcon: React.FC<IconProps> = function HelpIcon({ color, size, opacity}){
+  return (
+    <HelpSVG
+    fill={color || 'blue'}
+    width={size || 16}
+    height={size || 16}
+    opacity={opacity || 1}
     />
   );
 };
